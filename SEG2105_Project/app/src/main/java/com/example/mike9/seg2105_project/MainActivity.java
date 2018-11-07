4f5fda27840fb1d62733786e3d87cc342f3a1698
mikerayson <mike9251@rogers.com>
2018-11-07 18:18:12 -0500

    1st attempt at Firebase Authentication

--- SEG2105_Project/app/src/main/java/com/example/mike9/seg2105_project/MainActivity.java
+++ SEG2105_Project/app/src/main/java/com/example/mike9/seg2105_project/MainActivity.java
@@ -4,22 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
     }
+
     public void onClickLogin(View v){
         Intent login = new Intent(getApplicationContext(), WelcomeScreen.class);
         startActivity(login);
     }
-    //Should we add something that carries over the information that they already put in
-    //Aside from ^^^ this opens up the registration page
+
     public void onClickRegister(View v){
         Intent register = new Intent(getApplicationContext(), SignUp.class);
         startActivity(register);
     }
+
 }
--- SEG2105_Project/app/src/main/java/com/example/mike9/seg2105_project/SignUp.java
+++ SEG2105_Project/app/src/main/java/com/example/mike9/seg2105_project/SignUp.java
@@ -1,19 +1,48 @@
 package com.example.mike9.seg2105_project;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static java.security.AccessController.getContext;
 
 public class SignUp extends AppCompatActivity {
 
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
-    //will add the user to the database/array
-    public void addUser(){
-        return;
+    public void onClickConfirm(View v){
+        //Retrieves the email and password
+        EditText edt = (EditText)findViewById(R.id.Email);
+        EditText edt2 = (EditText)findViewById(R.id.Password);
+        String email = edt.getText().toString();
+        String password = edt2.getText().toString();
+        //Creates the user within firebase
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = mAuth.getCurrentUser();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Authentication Failed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
 
