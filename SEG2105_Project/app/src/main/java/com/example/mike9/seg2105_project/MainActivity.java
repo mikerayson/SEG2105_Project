1a4576bab9153ea744fb730af30e1f088d15cda9
mikerayson <mike9251@rogers.com>
2018-11-08 14:28:58 -0500

    Auth Working for Reals

--- SEG2105_Project/app/src/main/java/com/example/mike9/seg2105_project/MainActivity.java
+++ SEG2105_Project/app/src/main/java/com/example/mike9/seg2105_project/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -23,14 +24,22 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
     }
     public String getUserEmail(){
-        EditText et1 = (EditText)findViewById(R.id.Email);
+        EditText et1 = (EditText)findViewById(R.id.UsernameMain);
         String email = et1.getText().toString();
         return email;
     }
     public String getUserPass(){
-        EditText et2 = (EditText)findViewById(R.id.Password);
+        EditText et2 = (EditText)findViewById(R.id.PasswordMain);
         String password = et2.getText().toString();
         return password;
+    }
+    public void openSignup(){
+        Intent register = new Intent(getApplicationContext(), SignUp.class);
+        startActivity(register);
+    }
+    public void openWelcomePage(){
+        Intent login = new Intent(getApplicationContext(), WelcomeScreen.class);
+        startActivity(login);
     }
 
     public void onClickLogin(View v){
@@ -46,8 +55,7 @@ public class MainActivity extends AppCompatActivity {
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
                                 FirebaseUser user = mAuth.getCurrentUser();
-                                Intent login = new Intent(getApplicationContext(), WelcomeScreen.class);
-                                startActivity(login);
+                                openWelcomePage();
                             } else {
                                 Toast.makeText(MainActivity.this, "Authentication failed :(", Toast.LENGTH_LONG).show();
                             }
@@ -59,18 +67,18 @@ public class MainActivity extends AppCompatActivity {
     public void onClickRegister(View v) {
         String email = getUserEmail();
         String password = getUserPass();
+        
         if (email.isEmpty() || password.isEmpty()) {
             Toast.makeText(MainActivity.this, "Email/Password is Empty", Toast.LENGTH_SHORT).show();
-        } else {
-
+        }
+        else {
             mAuth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
                                 FirebaseUser user = mAuth.getCurrentUser();
-                                Intent register = new Intent(getApplicationContext(), SignUp.class);
-                                startActivity(register);
+                                openSignup();
                             } else {
                                 Toast.makeText(MainActivity.this, "Authentication failed :(", Toast.LENGTH_LONG).show();
                             }
