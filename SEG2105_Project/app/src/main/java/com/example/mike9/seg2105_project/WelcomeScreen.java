ab566091195529c85ea8d1b329724fdb3b0f3d72
Michael Rayson <mike9251@rogers.com>
2018-11-10 08:32:12 -0500

    Creation of services and rates, admin check not working

--- SEG2105_Project/app/src/main/java/com/example/mike9/seg2105_project/WelcomeScreen.java
+++ SEG2105_Project/app/src/main/java/com/example/mike9/seg2105_project/WelcomeScreen.java
@@ -1,13 +1,41 @@
 package com.example.mike9.seg2105_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class WelcomeScreen extends AppCompatActivity {
+
+    //Firebase things
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mRef;
+    private String userID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        //Carries over the user sign in
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mRef = mFirebaseDatabase.getReference();
+        userID = user.getUid();
     }
+
+    public void onClickAddService(View view){
+        Intent nextPage = new Intent(getApplicationContext(), AddService.class);
+        startActivity(nextPage);
+    }
+    //Put code to view active services here
 }
