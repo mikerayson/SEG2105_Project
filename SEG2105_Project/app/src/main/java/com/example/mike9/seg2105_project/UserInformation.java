a4897d2e1b70d9103d2de4f9688c85e98cd4eb84
mikerayson <mike9251@rogers.com>
2018-11-13 09:42:14 -0500

    Fixed WelcomeScreen text, added layout for Business Information

---
+++ SEG2105_Project/app/src/main/java/com/example/mike9/seg2105_project/UserInformation.java
@@ -0,0 +1,26 @@
+package com.example.mike9.seg2105_project;
+
+public class UserInformation {
+    private String firstname;
+    private String lastname;
+
+    public UserInformation(){
+
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
