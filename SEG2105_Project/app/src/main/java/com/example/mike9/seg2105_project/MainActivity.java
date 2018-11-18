92c5b396dc32d0b58951dcb54636f5ea9303ecf1
JulianoF <juliano1@live.ca>
2018-11-18 18:11:33 -0500

    Login Selects the Correct User Screen

--- SEG2105_Project/app/src/main/java/com/example/mike9/seg2105_project/MainActivity.java
+++ SEG2105_Project/app/src/main/java/com/example/mike9/seg2105_project/MainActivity.java
@@ -14,8 +14,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -73,27 +76,41 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-                                String userID;
+                                final String userID;
+
                                 FirebaseUser user = mAuth.getCurrentUser();
                                 mFirebaseDatabase = FirebaseDatabase.getInstance();
                                 userID = user.getUid();
+
                                 mRef = mFirebaseDatabase.getReference();
-                                mRef = mRef.child("Users");
 
+                                mRef = mRef.child("Users").child("Service Provider");
 
+                                mRef.addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        for(DataSnapshot ds : dataSnapshot.getChildren()){
+                                            String testID;
+                                            testID = ds.getKey();
 
-                                    if (userID.equals(adminID)) {
-                                        openAdminWelcomePage();
+                                            //DEBUGGING INFO///TO BE DELETED///
+                                            System.out.println(testID);
+                                            System.out.println(userID);
+                                            ///////////////////////////////////
 
-                                    } else if (false) {
-                                        openSPWelcomePage();
-
-                                    } else {
-                                        openUserWelcomePage();
-
+                                            if (userID.equals(adminID)) {
+                                                openAdminWelcomePage();
+                                            } else if (userID.equals(testID)) {
+                                                openSPWelcomePage();
+                                                break;
+                                            } else {
+                                                openUserWelcomePage();
+                                            }
+                                        }
                                     }
-
-
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {}
+                                });
                             } else {
                                 Toast.makeText(MainActivity.this, "Authentication failed :(", Toast.LENGTH_LONG).show();
                             }
