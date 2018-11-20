25334f2da359a429e872d2453d9809aa92fe55bd
unknown <jack9robinson@gmail.com>
2018-11-20 17:53:51 -0500

    adding and deleting sp services, buggy

--- SEG2105_Project/app/src/main/java/com/example/mike9/seg2105_project/SPWelcomeScreen.java
+++ SEG2105_Project/app/src/main/java/com/example/mike9/seg2105_project/SPWelcomeScreen.java
@@ -1,14 +1,25 @@
 package com.example.mike9.seg2105_project;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class SPWelcomeScreen extends AppCompatActivity {
 
@@ -19,11 +30,15 @@ public class SPWelcomeScreen extends AppCompatActivity {
     private DatabaseReference mRef;
     private String userID;
 
+    private String serviceName, serviceRate;
+    private ListView serviceList;
+    private ArrayList<String> array;
+
     private TextView txtView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        txtView=(TextView)findViewById(R.id.textView);
+        txtView = (TextView) findViewById(R.id.textView);
 
         //Carries over the user sign in
         mAuth = FirebaseAuth.getInstance();
@@ -32,9 +47,18 @@ public class SPWelcomeScreen extends AppCompatActivity {
         mRef = mFirebaseDatabase.getReference();
         userID = user.getUid();
 
+        serviceList = findViewById(R.id.service_list);
+        array = new ArrayList<>();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spwelcome_screen);
 
-        Toast.makeText(SPWelcomeScreen.this, "Hello Service Provider: "+userID, Toast.LENGTH_LONG).show();
+        //trying to display services provider has, needs to add service child in database
+
+    }
+
+    public void onClickAddServiceSP(View view){
+        Intent i = new Intent(SPWelcomeScreen.this, SPAddService.class);
+        startActivity(i);
     }
 }
